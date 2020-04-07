e37ea3c075c93110dd42c23480a50d5c884c5009
Anna Beketova <47106377+anya-mb@users.noreply.github.com>
2020-04-06 23:27:54 -0700

    Created utils.py

---
+++ bot_architecture/utils.py
@@ -0,0 +1,10 @@
+# Paths for all resources for the bot.
+RESOURCE_PATH = {
+    'RESNEXT_MODEL_PATH': 'models_path/resnext_2_6epoch_0.4047prec_0.3801rec',
+    'INCEPTION_MODEL_PATH': 'models_path/inception_3_16epoch_0.3587prec_0.3485rec',
+    'SAVE_IMAGES_PATH': 'bot_folder/downloaded_images/',
+    'TAG2IDX_PATH': 'tag2idx_small.json',
+    'LOGGER_FILE_PATH': 'bot_folder/logs.json',
+    'SHORT_LOGGER_FILE_PATH': 'bot_folder/short_logs.json',
+    'TAG2POSTS_PATH': 'tag2posts.json',
+}
